22cee0a7982eedd394eb14d01da3bf18bcd693e9
john paul Medina <medinajohnpaul01092000@gmail.com>
2023-03-08 22:04:53 +0800

    this version has the contact page done

---
+++ scripts.js
@@ -0,0 +1,30 @@
+function btnallproducts(){
+			document.getElementById("div1").style.display = 'block';
+			document.getElementById("div2").style.display = 'block';
+			document.getElementById("div3").style.display = 'block';
+			document.getElementById("div4").style.display = 'block';
+		}
+		function btnbread(){
+			document.getElementById("div1").style.display = 'block';
+			document.getElementById("div2").style.display = 'none';
+			document.getElementById("div3").style.display = 'none';
+			document.getElementById("div4").style.display = 'none';
+		}
+		function btncakes(){
+			document.getElementById("div1").style.display = 'none';
+			document.getElementById("div2").style.display = 'block';
+			document.getElementById("div3").style.display = 'none';
+			document.getElementById("div4").style.display = 'none';
+		}
+		function btnnoodles(){
+			document.getElementById("div1").style.display = 'none';
+			document.getElementById("div2").style.display = 'none';
+			document.getElementById("div3").style.display = 'block';
+			document.getElementById("div4").style.display = 'none';
+		}
+		function btndishes(){
+			document.getElementById("div1").style.display = 'none';
+			document.getElementById("div2").style.display = 'none';
+			document.getElementById("div3").style.display = 'none';
+			document.getElementById("div4").style.display = 'block';
+		}
